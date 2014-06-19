efd2d6e04f2502c5fa681809315ac8b1ac96a345
Dan Murphy <murphydbuffalo@gmail.com>
2014-06-19 13:56:11 -0400

    Added JS functions with JQuery, which  append either an X or O CSS pseudo-element to a square whenever it is clicked.

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -1,1 +1,24 @@
-// Your code here
+var playerToken = 'x';
+
+function addClass(square, token) {
+	square.addClass('square-' + token);
+}
+
+function assignToken(token) {
+	if(token === 'x') {
+  	return token = 'o';
+  } else {
+  	  return token = 'x';
+    }
+  
+}
+
+$('.board').on('click', ".square:not('.square-x, .square-o')", function(event) {
+	var $chosenSquare = $(event.currentTarget);
+	addClass($chosenSquare, playerToken);
+	playerToken = assignToken(playerToken);
+});
+  
+  
+  
+
